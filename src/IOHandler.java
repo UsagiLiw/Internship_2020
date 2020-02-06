d21ff4c5bcf5ad598817f1bf25f27444aba1b68f
UsagiLiw <usagiliw@gmail.com>
2020-02-06 12:27:38 +0700

    Initial commit

---
+++ src/IOHandler.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+/**
+ * This class provides method of receive input from terminal from user.
+ *
+ *  Created by Nonthakorn Sukprom, 5 February 2020
+ *  Used for The internship 2020 test assignment.
+ */
+public class IOHandler {
+    /**
+     * Scanner object for handling input.
+     */
+    private static Scanner scn = new Scanner(System.in);
+
+    /**
+     * Asks for a string and returns to the caller.
+     * @param prompt    String to print, asking for input.
+     * @return          the string user entered.
+     */
+    public static String getString (String prompt){
+        System.out.println(prompt);
+        String value = scn.nextLine();
+        return value;
+    }
+
+    /**
+     * Asks for a integer and returns to the caller.
+     * @param prompt    String to print, asking for input.
+     * @return          the integer user entered.
+     */
+    public static int getInteger (String prompt){
+        System.out.println(prompt);
+        int value = scn.nextInt();
+        /*Fix the problem that the terminal's cursor does not move to the next line after read input*/
+        scn.nextLine();
+        return value;
+    }
+
+    /**
+     * Asks for a double and returns to the caller.
+     * @param prompt    String to print, asking for input.
+     * @return          the double user entered.
+     */
+    public static double getDouble (String prompt){
+        System.out.println(prompt);
+        double value = scn.nextDouble();
+        /*Fix the problem that the terminal's cursor does not move to the next line after read input*/
+        scn.nextLine();
+        return value;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean bContinue = true;
+        while(bContinue) {
+            int number = IOHandler.getInteger("Enter number of names: ");
+            if (number <= 0){
+                System.out.println("Number must be more than 0!");
+                continue;
+            }
+            ArrayList<String> name = new ArrayList<String>();
+            for (int i = 0 ; i < number ; i++){
+                String newName = IOHandler.getString("Enter name #" + (i + 1) + ": ");
+                name.add(newName);
+            }
+            //Acronymization and sorting
+            
+            System.out.println();
+            for (int i = 0 ; i < number ; i++) {
+                System.out.println(name.get(i));
+            }
+            String exit = IOHandler.getString("Do you want to exit? (y/n): ");
+            if (exit.startsWith("Y") || exit.startsWith("y")){
+                System.out.println("Exit ... ");
+                System.exit(0);
+            }
+        }
+    }
+}
